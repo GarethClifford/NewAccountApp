cd961b4eb6f21419b2cd1f82d4020766c890d324
GarethClifford <gareth.clifford@live.co.uk>
2018-10-30 11:30:16 +0000

    Commit

---
+++ accountapp/src/main/java/com/qa/app/Account.java
@@ -0,0 +1,18 @@
+package com.qa.app;
+
+public class Account extends Person{
+
+	
+	public Account() {
+		
+	}
+	
+	public Account(String firstname, String lastname, String accountnumber) {
+		super.firstName = firstname;
+		super.lastName = lastname;
+		super.accountNumber = accountnumber;
+		
+	}
+	
+	
+}
